190ce1368bc8fc97f6e9773ba8a78f5a67441453
ibraram100 <ibraram100@hotmail.com>
2023-06-05 23:28:52 +0200

    Fixed some bugs in sign up

--- html/sign_up.php
+++ html/sign_up.php
@@ -5,24 +5,47 @@
 //  }   
 
  extract($_POST, EXTR_PREFIX_ALL, 'var');
+ $counter = 0;
  if (empty($var_first_name))
  {
-    echo "<h1 style='color:red;'>First name is required !</h1>";
+   echo "<h1 style='color:red;'>First name is required !</h1>";
+   $counter++;
+
  }
  
  if (empty($var_username))
  {
-    echo "<h1 style='color:red;'>Username is required !</h1>";
+   echo "<h1 style='color:red;'>Username is required !</h1>";
+   $counter++;
  }
+ 
  if (empty($var_password))
  {
-    echo "<h1 style='color:red;'>Password is required !</h1>";
+   echo "<h1 style='color:red;'>Password is required !</h1>";
+   $counter++;
  }
 
  if (empty($var_confirm_password))
  {
-    echo "<h1 style='color:red;'>Please confirm password !</h1>";
+   echo "<h1 style='color:red;'>Please confirm password !</h1>";
+   $counter++;
  }
+ else if ($var_confirm_password != $var_password)
+ {
+   echo "<h1 style='color:red;'>Passwords do not match !</h1>";
+ }
+ if ($counter>0)
+ {
+   echo "<button><a href='SignUp.html'>Go Back</a></button>";
+ }
+ if ($counter==0)
+ {
+   echo "<H1 style='color:green';>You have been successfully registered !";
+ }
+
+
+
+ 
  
  
 
