d950a1d20fc6adc3d401878f55069821fc07b612
ibraram100 <ibraram100@hotmail.com>
2023-06-30 22:50:34 +0200

    added categories.php

---
+++ php/categories.php
@@ -0,0 +1,43 @@
+<!-- ابراهيم محمد فاتح بن رمضان -->
+<!-- 2023/06/30 -->
+<!-- Allowing logged in users to add new categories -->
+
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Fonts  -->
+        <!-- roboto -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <!-- Inter -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <!-- Styles  -->
+        <link rel="stylesheet" href="../styles/styles.css">
+        <!-- Title  -->
+        <title>Expense Tracker Sign Up</title>
+        <!-- Favicon -->
+        <link rel="icon" href="../images/shroom wojak.png" type="image/x-icon">
+    </head>
+    <!-- Header links -->
+    <header>
+        <div class="logo">
+            <a href="home.html"><img src="../images/shroom wojak.png" alt="Logo" class="logo_img">Expense Tracker</a>
+        </div>
+          <nav>
+            <ul>
+              <li><a href="home.html">Home</a></li>
+              <li><a href="#">Categories</a></li>
+              <li><a href="login.html">Login</a></li>
+              <li><a href="#">Log Out</a></li>
+            </ul>
+          </nav>
+        </header>
+</html>
